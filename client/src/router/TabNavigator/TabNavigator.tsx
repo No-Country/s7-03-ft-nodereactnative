74868a4ee9336c4eb9c627a160cf19958cca27a6
Francisco Rey <reyf1998@outlook.es>
2023-03-27 13:05:17 -0300

    add icons

--- client/src/router/TabNavigator/TabNavigator.tsx
+++ client/src/router/TabNavigator/TabNavigator.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const TabNavigator = () => {
     let test: string = 'Esta el tab navigator';
@@ -7,6 +8,7 @@ const TabNavigator = () => {
     return (
         <View style={{ marginTop: 50 }}>
             <Text>Test tab navigator</Text>
+            <Icon name="star-outline" size={50} color="black" />
         </View>
     );
 };
