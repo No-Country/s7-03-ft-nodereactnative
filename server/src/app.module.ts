994df4e41f32348cad34b3d10089999250b8782e
Jared Mejia <jaredmejia4440@gmail.com>
2023-03-30 16:53:09 -0600

    fix imports errors

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,5 @@
 import { TransformInterceptor } from './interceptors/transform.interceptor';
 import { Module } from '@nestjs/common';
-import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
@@ -8,7 +7,6 @@ import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { UsersModule } from './users/users.module';
 import { ProductCategoriesService } from './product-categories/product-categories.service';
-import { ProductCategoriesController } from './product-categories/product-categories.controller';
 import { ProductCategoriesModule } from './product-categories/product-categories.module';
 
 @Module({
@@ -20,13 +18,9 @@ import { ProductCategoriesModule } from './product-categories/product-categories
     ProductCategoriesModule,
   ],
   providers: [
-    //PrismaService,
     { provide: APP_GUARD, useClass: JwtAuthGuard },
     { provide: APP_INTERCEPTOR, useClass: TransformInterceptor },
     ProductCategoriesService,
-  ],
-  controllers: [
-    /* ProductCategoriesController */
   ],
 })
 export class AppModule {}
