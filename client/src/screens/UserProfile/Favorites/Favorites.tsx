38947bcace6333d5d7e9a671eee7f1901fb49ac0
Francisco Rey <reyf1998@outlook.es>
2023-04-12 12:37:51 -0300

    add slices

---
+++ client/src/screens/UserProfile/Favorites/Favorites.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Favorites = () => {
+    return (
+        <View>
+            <Text>Produtos y tiendas favoritas</Text>
+        </View>
+    );
+};
+
+export default Favorites;
