6f43c1c1a8aa34e265ed290430b2c051f235260c
Emanuel-Capo <emanuel_c400@hotmail.com>
2023-04-13 16:11:30 -0300

    map vet view

--- client/src/screens/Maps/Maps.tsx
+++ client/src/screens/Maps/Maps.tsx
@@ -5,7 +5,7 @@ import React, {
     useRef,
     useState,
 } from 'react';
-import { Button, Image, Text, View } from 'react-native';
+import { Image, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import styled from 'styled-components/native';
 import { useGetVeterinariesQuery } from '../../reduxApp/services/veterinaries/vetServices';
@@ -14,15 +14,40 @@ import BottomSheet, { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { useSelector } from 'react-redux';
 import { PosState } from '../../reduxFeature/user/userPositionSlice';
 import { VetPosState } from '../../reduxFeature/veterinaries/vetPositionSlice';
+import { ButtonPrimary } from '../../components';
+import { FontAwesome } from '@expo/vector-icons';
 
 const Map = styled(MapView)`
     height: 100%;
     width: 100%;
 `;
 
+const RowView = styled.View`
+    flex-direction: row;
+    margin: 5px;
+    `;
+
+const ContainView = styled.View`
+    padding: 0 20px;
+    `;
+
+const TextView = styled.Text`
+    margin-left: 10px;
+    `;
+
+const TextTitle = styled.Text`
+    margin-left: 10px;
+    font-weight: bold;
+    font-size: 18px;
+`;
+
 const Maps = () => {
-    const positionSelector = useSelector((state:PosState)=> state.userPositionSlice)
-    const initialSelector = useSelector((state:VetPosState)=> state.vetPositionSlice)
+    const positionSelector = useSelector(
+        (state: PosState) => state.userPositionSlice
+    );
+    const initialSelector = useSelector(
+        (state: VetPosState) => state.vetPositionSlice
+    );
 
     const [origin, setOrigin] = useState({
         latitude: 0,
@@ -41,30 +66,31 @@ const Maps = () => {
 
     const { data } = useGetVeterinariesQuery('');
 
-    
     useEffect(() => {
-        if (data) setVeterinaries(data.results.veterinaries);      
-    }, [data])
+        if (data) setVeterinaries(data.results.veterinaries);
+    }, [data]);
 
     useEffect(() => {
-        if (positionSelector.latitude && positionSelector.longitude){
+        if (positionSelector.latitude && positionSelector.longitude) {
             setOrigin({
                 latitude: positionSelector.latitude,
                 longitude: positionSelector.longitude,
-            })
-            setMapOk(true)
+            });
+            setMapOk(true);
         }
-    }, [])
-    
-    
+    }, []);
 
     return (
         <BottomSheetModalProvider>
             {mapOk && (
                 <Map
                     initialRegion={{
-                        latitude: initialSelector.latitude ? initialSelector.latitude : origin.latitude,
-                        longitude: initialSelector.longitude ? initialSelector.longitude : origin.longitude,
+                        latitude: initialSelector.latitude
+                            ? initialSelector.latitude
+                            : origin.latitude,
+                        longitude: initialSelector.longitude
+                            ? initialSelector.longitude
+                            : origin.longitude,
                         latitudeDelta: 0.1,
                         longitudeDelta: 0.1,
                     }}
@@ -77,8 +103,10 @@ const Maps = () => {
                                 latitude: vets.latitude,
                                 longitude: vets.longitude,
                             }}
-                            onPress={() => {handleSheetChanges(0)
-                            setVeterinary(vets)}}
+                            onPress={() => {
+                                handleSheetChanges(1);
+                                setVeterinary(vets);
+                            }}
                         >
                             <Image
                                 source={require('../../../assets/veterinaryLoc.webp')}
@@ -97,15 +125,60 @@ const Maps = () => {
                 enablePanDownToClose
                 onChange={handleSheetChanges}
             >
-                <View>
-                    {veterinary && <>
-                    <Text>Nombre: {veterinary.name}</Text>
-                    <Text>Teléfono: {veterinary.phone}</Text>
-                    <Text>Dirección: {veterinary.address}</Text>
-                    <Text>{veterinary.description}</Text>
-                    <Button title='Ir a la tienda' />
-                    </>}
-                </View>
+                <ContainView>
+                    {veterinary && (
+                        <>
+                            <RowView>
+                                <Image
+                                    source={require('../../../assets/vetImage.png')}
+                                    style={{ width: 70, height: 70 }}
+                                />
+                                <View>
+                                <TextTitle>{veterinary.name}</TextTitle>
+                                <RowView>
+                                <FontAwesome name="star" size={15} color="black" />
+                                <FontAwesome name="star" size={15} color="black" />
+                                <FontAwesome name="star" size={15} color="black" />
+                                <FontAwesome name="star-half-empty" size={15} color="black" />
+                                <FontAwesome name="star-o" size={15} color="black" />
+                                </RowView>
+                                </View>
+                            </RowView>
+                            <RowView>
+                                <FontAwesome
+                                    name="map-marker"
+                                    size={24}
+                                    color="black"
+                                />
+                                <TextView>{veterinary.address}</TextView>
+                            </RowView>
+                            <RowView>
+                                <FontAwesome
+                                    name="clock-o"
+                                    size={24}
+                                    color="black"
+                                />
+                                <TextView>
+                                    Abre a las 09:30 hasta 18:00
+                                </TextView>
+                            </RowView>
+                            <RowView>
+                                <FontAwesome
+                                    name="phone"
+                                    size={24}
+                                    color="black"
+                                />
+                                <TextView>{veterinary.phone}</TextView>
+                            </RowView>
+                            <TextView>{veterinary.description}</TextView>
+                            <TextView></TextView>
+                            <ButtonPrimary
+                                onPress={() => console.log('')}
+                                title="Ir a la tienda"
+                            />
+                        </>
+                    )}
+                </ContainView>
             </BottomSheet>
         </BottomSheetModalProvider>
     );
