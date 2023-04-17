72f89f20884f905b33a92cd587fa53c425ca7646
Emanuel-Capo <emanuel_c400@hotmail.com>
2023-04-17 15:57:30 -0300

    petshop1

--- client/src/screens/Maps/Maps.tsx
+++ client/src/screens/Maps/Maps.tsx
@@ -1,8 +1,3 @@
-import {
-    requestForegroundPermissionsAsync,
-    getCurrentPositionAsync,
-    Accuracy,
-} from 'expo-location';
 import React, {
     useCallback,
     useEffect,
@@ -14,7 +9,6 @@ import { Image, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import styled from 'styled-components/native';
 import { useGetVeterinariesQuery } from '../../reduxApp/services/veterinaries/vetServices';
-import { VetInterface } from '../../interfaces/vetInterfaces';
 import BottomSheet, { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { useSelector } from 'react-redux';
 import { PosState } from '../../reduxFeature/user/userPositionSlice';
@@ -23,7 +17,6 @@ import { ButtonPrimary } from '../../components';
 import { FontAwesome } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { RootStackParamList } from '../../constants/types';
 import { VetBarProps } from '../../components/VeterinariasBar/VeterinariasBar';
 
 const Map = styled(MapView)`
@@ -51,6 +44,7 @@ const TextTitle = styled.Text`
 `;
 
 const Maps = () => {
+   
     const navigation = useNavigation<StackNavigationProp<any, 'Veterinarias'>>();
 
     const positionSelector = useSelector(
@@ -61,61 +55,43 @@ const Maps = () => {
     );
 
     const [origin, setOrigin] = useState({
-        latitude: -41.965159,
-        longitude: -71.535335,
+        latitude: 0,
+        longitude: 0,
     });
     const [mapOk, setMapOk] = useState(false);
     const [veterinaries, setVeterinaries] = useState<VetBarProps[]>([]);
     const [vet, setVeterinary] = useState<VetBarProps>();
 
-    const [isOpen, setIsOpen] = useState(true);
     const bottomSheetRef = useRef<BottomSheet>(null);
     const snapPoints = useMemo(() => ['25%', '50%', '75%'], []);
     const handleSheetChanges = useCallback((index: number) => {
         bottomSheetRef.current?.snapToIndex(index);
-        console.log('handleSheetChanges', index);
+        // console.log('handleSheetChanges', index);
     }, []);
 
     const { data } = useGetVeterinariesQuery('');
 
-    const getLocationPermission = async () => {
-        const { status } = await requestForegroundPermissionsAsync();
-
-        if (status !== 'granted') {
-            alert('Permiso denegado');
-            return;
-        }
-        await getCurrentPositionAsync({accuracy: Accuracy.Lowest})
-            .then((pos) => {
-                setOrigin({
-                    latitude: pos.coords.latitude,
-                    longitude: pos.coords.longitude,
-                });
-                console.log(pos);
-                
-            })
-            .catch((e) => console.log(e));
-            setMapOk(true);
-            console.log(veterinaries);
-            
-        };
-
-        useEffect(() => {
-        getLocationPermission();
-    }, []);
-    
     useEffect(() => {
-        if (data) setVeterinaries(data.results.veterinaries);      
-    }, [data])
-    
+        if (data) setVeterinaries(data.results.veterinaries);
+    }, [data]);
+
+    useEffect(() => {
+        if (positionSelector.latitude && positionSelector.longitude) {
+            setOrigin({
+                latitude: positionSelector.latitude,
+                longitude: positionSelector.longitude,
+            });
+            setMapOk(true);
+        }
+    }, []);
 
     return (
         <BottomSheetModalProvider>
             {mapOk && (
                 <Map
                     initialRegion={{
-                        latitude: origin.latitude,
-                        longitude: origin.longitude,
+                        latitude: initialSelector.latitude ? initialSelector.latitude : origin.latitude,
+                        longitude: initialSelector.longitude ? initialSelector.longitude :origin.longitude,
                         latitudeDelta: 0.1,
                         longitudeDelta: 0.1,
                     }}
