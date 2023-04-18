a080f10c1a03c24396a717829eef05b9b1a43f03
Francisco Rey <reyf1998@outlook.es>
2023-04-18 13:48:58 -0300

    styles finish

--- client/src/screens/Cart/RenderProduct.tsx
+++ client/src/screens/Cart/RenderProduct.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import { View, Text, Image, Button, TouchableOpacity } from 'react-native';
+import {
+    View,
+    Text,
+    Image,
+    Button,
+    TouchableOpacity,
+    Modal,
+    StyleSheet,
+} from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { useState } from 'react';
 
 const RenderProduct = () => {
+    const [openOptions, setOpenOptions] = useState(false);
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const toggleModal = () => {
+        setIsModalVisible(!isModalVisible);
+    };
     return (
         <View
             style={{
@@ -12,7 +26,7 @@ const RenderProduct = () => {
                 padding: 5,
                 marginTop: 5,
                 height: 120,
-                backgroundColor: '#ebebeb',
+                backgroundColor: '#f8f8f8',
             }}
         >
             <Image
@@ -67,7 +81,123 @@ const RenderProduct = () => {
                 </TouchableOpacity>
             </View>
             <View>
-                <Ionicons size={30} name="ellipsis-vertical" />
+                <TouchableOpacity onPress={toggleModal}>
+                    <Ionicons size={30} name="ellipsis-vertical" />
+                </TouchableOpacity>
+                <Modal
+                    transparent
+                    visible={isModalVisible}
+                    onRequestClose={toggleModal}
+                >
+                    <TouchableOpacity
+                        style={{
+                            flex: 1,
+                            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                            justifyContent: 'flex-end',
+                        }}
+                        onPress={toggleModal}
+                    >
+                        <View
+                            style={{
+                                backgroundColor: '#fff',
+                                width: '100%',
+                                height: '50%',
+                            }}
+                        >
+                            <View>
+                                <View
+                                    style={{
+                                        marginTop: 20,
+                                        flexDirection: 'row',
+                                        justifyContent: 'space-between',
+                                        marginHorizontal: 35,
+                                        marginBottom: 20,
+                                    }}
+                                >
+                                    <Text
+                                        style={{
+                                            textTransform: 'uppercase',
+                                            fontWeight: '500',
+                                            fontSize: 18,
+                                        }}
+                                    >
+                                        Opciones
+                                    </Text>
+                                    <TouchableOpacity onPress={toggleModal}>
+                                        <Ionicons size={30} name="close" />
+                                    </TouchableOpacity>
+                                </View>
+                                <View
+                                    style={{
+                                        flexDirection: 'row',
+                                        alignItems: 'center',
+                                        gap: 5,
+                                        marginHorizontal: 35,
+
+                                        marginVertical: 15,
+                                    }}
+                                >
+                                    <Ionicons
+                                        size={30}
+                                        name="add-circle-outline"
+                                    />
+                                    <Text>Editar cantidad</Text>
+                                </View>
+                                <View
+                                    style={{
+                                        borderBottomWidth: 1,
+                                        borderColor: 'black',
+                                        marginVertical: 5,
+                                    }}
+                                />
+                                <View
+                                    style={{
+                                        flexDirection: 'row',
+                                        alignItems: 'center',
+                                        gap: 5,
+                                        marginHorizontal: 35,
+
+                                        marginVertical: 15,
+                                    }}
+                                >
+                                    <Ionicons
+                                        size={30}
+                                        name="md-bookmark-outline"
+                                    />
+                                    <Text>Mover a favoritos</Text>
+                                </View>
+                                <View
+                                    style={{
+                                        borderBottomWidth: 1,
+                                        borderColor: 'black',
+                                        marginVertical: 5,
+                                    }}
+                                />
+                                <View
+                                    style={{
+                                        flexDirection: 'row',
+                                        alignItems: 'center',
+                                        gap: 5,
+                                        marginHorizontal: 35,
+                                        marginVertical: 15,
+                                    }}
+                                >
+                                    <Ionicons
+                                        size={30}
+                                        name="trash-bin-outline"
+                                    />
+                                    <Text>Eliminar del carrito</Text>
+                                </View>
+                                <View
+                                    style={{
+                                        borderBottomWidth: 1,
+                                        borderColor: 'black',
+                                    }}
+                                />
+                            </View>
+                        </View>
+                    </TouchableOpacity>
+                </Modal>
             </View>
         </View>
     );
